cc3511086516ffeb1d20e6e1347966595e922a02
mellowc4ndy <mellowc4ndy@gmail.com>
2026-02-12 17:31:02 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-dream-home/', // GitHub Repository 이름과 일치해야 합니다.
+  // base를 './'로 설정하면 어떤 저장소 이름으로 배포하든 경로 문제가 생기지 않습니다.
+  base: './', 
+  build: {
+    outDir: 'dist',
+  }
 });
