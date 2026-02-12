c6db8d6b3ecce0561298e8c9d07936be417be40d
mellowc4ndy <mellowc4ndy@gmail.com>
2026-02-12 17:12:16 +0900

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // 스타일 파일 불러오기
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Quicksand', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
