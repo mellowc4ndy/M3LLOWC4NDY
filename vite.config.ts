9eb3430ea981cdfe1223a6d2107c49eb418876f8
mellowc4ndy <mellowc4ndy@gmail.com>
2026-02-12 17:03:38 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/M3LLOWC4NDY/', // GitHub Repository 이름과 일치해야 합니다.
+  base: '/my-dream-home/', // GitHub Repository 이름과 일치해야 합니다.
 });
